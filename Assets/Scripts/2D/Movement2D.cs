98c996e728d87c78f1d1ee77f8985cc21ef66b91
Gabrielis <zabitisgabrielis@gmail.com>
2024-09-12 11:50:47 +0300

    2d and fake 3d perspectives
    This is terrible btw

---
+++ Assets/Scripts/2D/Movement2D.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement2D : MonoBehaviour
+{
+    [SerializeField] private float m_MovementSpeed = 1.0f;
+    [SerializeField] private float m_JumpForce = 5.0f;
+
+    private Rigidbody m_RigidBody = default;
+
+    private void Awake()
+    {
+        m_RigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_RigidBody.velocity = new Vector3(m_MovementSpeed, m_RigidBody.velocity.y, m_RigidBody.velocity.z);
+            transform.rotation = Quaternion.identity;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            m_RigidBody.velocity = new Vector3(-m_MovementSpeed, m_RigidBody.velocity.y, m_RigidBody.velocity.z);
+            transform.rotation = Quaternion.Euler(new Vector2(0.0f, -180.0f));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, m_RigidBody.velocity.y, - m_MovementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, m_RigidBody.velocity.y, m_MovementSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && m_RigidBody.velocity.y == 0.0f)
+        {
+            m_RigidBody.velocity = Vector2.up * m_JumpForce;
+        }
+    }
+}
