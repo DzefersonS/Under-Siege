2fe32c6c0220f473920012d332d79753a54acbf3
Gabrielis <zabitisgabrielis@gmail.com>
2024-09-24 10:29:25 +0300

    Remove 3D, fix 2D scene

--- Assets/Scripts/2D/Movement2D.cs
+++ Assets/Scripts/2D/Movement2D.cs
@@ -2,39 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement2D : MonoBehaviour
 {
     [SerializeField] private float m_MovementSpeed = 1.0f;
     [SerializeField] private float m_JumpForce = 5.0f;
 
-    private Rigidbody m_RigidBody = default;
+    private Rigidbody2D m_RigidBody = default;
 
     private void Awake()
     {
-        m_RigidBody = GetComponent<Rigidbody>();
+        m_RigidBody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            m_RigidBody.velocity = new Vector3(m_MovementSpeed, m_RigidBody.velocity.y, m_RigidBody.velocity.z);
+            m_RigidBody.velocity = new Vector3(m_MovementSpeed, m_RigidBody.velocity.y);
             transform.rotation = Quaternion.identity;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            m_RigidBody.velocity = new Vector3(-m_MovementSpeed, m_RigidBody.velocity.y, m_RigidBody.velocity.z);
+            m_RigidBody.velocity = new Vector3(-m_MovementSpeed, m_RigidBody.velocity.y);
             transform.rotation = Quaternion.Euler(new Vector2(0.0f, -180.0f));
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, m_RigidBody.velocity.y, - m_MovementSpeed);
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, m_RigidBody.velocity.y, m_MovementSpeed);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && m_RigidBody.velocity.y == 0.0f)
