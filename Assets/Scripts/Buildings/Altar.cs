62e0f20251dbd58e4cad7a1a24ae8fece3d0ea12
Aleksandras Bukelis <aleksbukelis@gmail.com>
2024-10-27 16:31:17 +0200

    Added shop UI appearing when player touches Altar

---
+++ Assets/Scripts/Buildings/Altar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : MonoBehaviour
+{
+    [SerializeField] private Canvas _shopCanvas;
+
+    void Start()
+    {
+        _shopCanvas = GameObject.Find("ShopCanvas").GetComponent<Canvas>();
+
+        if (_shopCanvas != null)
+            _shopCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _shopCanvas.enabled = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _shopCanvas.enabled = false;
+
+        }
+    }
+}
