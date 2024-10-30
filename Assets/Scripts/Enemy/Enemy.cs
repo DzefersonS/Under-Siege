e18bec0ea66f2fc34724d064e6de710899e6e3ff
Gabrielis <zabitisgabrielis@gmail.com>
2024-10-30 18:19:06 +0200

    Primitive enemy state machine

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,12 +5,36 @@ public class Enemy : Poolable
 {
     [SerializeField] private int m_Health;
     [SerializeField] private EnemyEventSO m_EnemyDeathEventSO;
+    [SerializeField] private State[] m_EnemyStates;
 
+    private enum EEnemyState
+    {
+        Moving = 0,
+        Attacking,
+        Dying,
+        COUNT
+    }
+
+    private EEnemyState m_CurrentState = default;
     private int m_CurrentHealth = 0;
 
     public override void Initialize()
     {
         m_CurrentHealth = m_Health;
+        transform.rotation = Quaternion.Euler(0.0f, transform.position.x > 0 ? 180.0f : 0.0f, 0.0f);
+        ChangeState(EEnemyState.Moving);
+    }
+
+    private void Update()
+    {
+        m_EnemyStates[(int)m_CurrentState].UpdateState(Time.deltaTime);
+    }
+
+    private void ChangeState(EEnemyState newState)
+    {
+        m_EnemyStates[(int)m_CurrentState]?.ExitState();
+        m_CurrentState = newState;
+        m_EnemyStates[(int)m_CurrentState].EnterState();
     }
 
     public void TakeDamage(int damage)
