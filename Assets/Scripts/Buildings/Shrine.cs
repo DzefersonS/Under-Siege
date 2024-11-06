d86997c2807e0a712742045f4855fb6e7c29ce76
Gabrielis <zabitisgabrielis@gmail.com>
2024-11-07 01:23:43 +0200

    Made shrine attackable
    - Shrine can be attacked
    - Has health
    - HP bar reflects health

--- Assets/Scripts/Buildings/Shrine.cs
+++ Assets/Scripts/Buildings/Shrine.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shrine : MonoBehaviour
+public class Shrine : MonoBehaviour, IAttackable
 {
+    [SerializeField] private int m_MaxHealth;
     [SerializeField] private GameObjectEventSO _gameObjectEventSO;
+    [SerializeField] private RectTransform m_Healthbar;
 
+    private Vector2 m_HealthbarSizeDelta = default;
+    private float m_HealthbarWidth = default;
+    private float m_HealthbarWidthFactor = default;
+    private int m_CurrentHealth = default;
+
+    private void Awake()
+    {
+        m_HealthbarWidth = m_Healthbar.sizeDelta.x;
+        m_HealthbarSizeDelta = m_Healthbar.sizeDelta;
+        m_CurrentHealth = m_MaxHealth;
+        m_HealthbarWidthFactor = m_HealthbarWidth / m_MaxHealth;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -20,5 +34,12 @@ public class Shrine : MonoBehaviour
         if (other.tag == "Player")
             _gameObjectEventSO.value = this.gameObject;
 
+    }
+
+    public void Damage(int damageAmount)
+    {
+        m_CurrentHealth -= damageAmount;
+        m_HealthbarSizeDelta.x = m_CurrentHealth * m_HealthbarWidthFactor;
+        m_Healthbar.sizeDelta = m_HealthbarSizeDelta;
     }
 }
