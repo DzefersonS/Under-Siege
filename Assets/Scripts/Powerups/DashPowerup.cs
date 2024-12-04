a73735d0e6a1e198d860a4280182a284faff5a4a
Gabrielis <zabitisgabrielis@gmail.com>
2024-12-04 21:23:34 +0200

    Add dash powerup

---
+++ Assets/Scripts/Powerups/DashPowerup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashPowerup : PowerupBase
+{
+    [SerializeField] private InputBasedMovement m_Player;
+    [SerializeField] private Animator m_PlayerAnimator;
+    [SerializeField] private float m_DashDistance;
+
+    protected override void ActivatePowerup()
+    {
+        float direction = m_Player.transform.rotation.y == 0 ? 1 : -1;
+        m_Player.transform.position += new Vector3(m_DashDistance * direction, 0.0f, 0.0f);
+        // m_PlayerAnimator.Play("Dash");
+
+        StartCooldown();
+    }
+}
